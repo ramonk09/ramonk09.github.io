47593184cec787ea05fa7ecdbc81dad0b55ac510
ramonk09 <46895061+ramonk09@users.noreply.github.com>
2019-09-08 23:34:15 -0400

    Fin

--- javascript/jsCode.js
+++ javascript/jsCode.js
@@ -16,4 +16,12 @@ function myFunction() {
   // var total = x+y;
    //return total;
   }
-
+  function receipt() {
+    var x =parseFloat( document.getElementById("CPUCost").value);
+    
+    var y =parseFloat( document.getElementById("StorageCost").value);  
+    var z =parseFloat( document.getElementById("MonitorCost").value); 
+    var subT= (x + y+ z)*1.115;
+  var fixed = subT.toFixed(2);
+    window.alert("Thanks for buying with us!: "+ "\n" + "Sub Total"+ (x+y+z) + "\n"+"Total" + fixed + "\nHave a nice day!");
+  }
